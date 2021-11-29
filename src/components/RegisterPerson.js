33edc5206d60e0e44de67b98b1c753d4544cf040
Edxon Meneses <edxon.meneses@gmail.com>
2021-11-29 11:39:03 -0500

    Input fields added in the form

--- src/components/RegisterPerson.js
+++ src/components/RegisterPerson.js
@@ -5,24 +5,55 @@ const RegisterPerson = () => {
   return (
     <div className="d-flex justify-content-center">
       <div className="card ">
-        <h5 className="card-header">Featured</h5>
+        <h5 className="card-header">Register a patient</h5>
         <div className="card-body">
-          <h5 className="card-title">Special title treatment</h5>
+          {/* <h5 className="card-title">Special title treatment</h5> */}
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
+            <Form.Group className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" />
+              {/* <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
-              </Form.Text>
+              </Form.Text> */}
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group className="mb-3">
+              <Form.Label>Phone number</Form.Label>
+              <Form.Control type="text" placeholder="Phone number" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+
+            <Form.Group className="mb-3">
+              <Form.Label>Vaccine applied</Form.Label>
+              <Form.Control type="text" placeholder="Name of the vaccine" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 d-flex justify-content-between">
+              <Form.Label>Dosis applied:</Form.Label>
+              <Form.Check
+                label="One dosis"
+                type="radio"
+                name="dosis"
+                value="one dosis"
+                checked
+              />
+              <Form.Check
+                label="Two dosis"
+                type="radio"
+                name="dosis"
+                value="two dosis"
+              />
+              <Form.Check
+                label="Three dosis"
+                type="radio"
+                name="dosis"
+                value="three dosis"
+              />
+              <Form.Check
+                label="None"
+                type="radio"
+                name="dosis"
+                value="cero dosis"
+              />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
